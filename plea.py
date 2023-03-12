9bae25322b5218e5a4b438c6a85468b495e0374b
Henry Ndubuaku <ndubuakuhenry@gmail.com>
2023-03-12 17:10:10 +0000

    Update plea.py

--- plea.py
+++ plea.py
@@ -12,18 +12,9 @@ class PLEA:
         self.text_parser = spacy.load(text_processing_model)
         self.sentence_embedding_model = SentenceTransformer(text_embedding_model)
         self.mask_filling_model = pipeline('fill-mask', model=mask_filling_model)
-        self.enhanced_texts = []
-
-        """
-        TAG  NAME                   EXAMPLE                     ENHANCEMENT     EXAMPLE
-        JJR  Comparative Adjective  this is 'better'            insert before   this is 'significantly better'
-        PDT  Predeterminer          this is 'half' the journey  insert before   this is 'only half' the journey
-        RBR  Comparative Adverb     she slept 'longer'          insert before   she slept 'relatively longer'
-        MD   Modal Auxillary Verb   I 'can' call you            replace         I 'could' call you
-        """
-
         self.tags_to_enhance = set(["JJR", "PDT", "RBR"])
         self.tags_to_replace = set(["MD"])
+        self.enhanced_texts = []
 
 
     def enhance_text(self, text, top_k=1, confidence=0.2, tolerance=0.9):
@@ -42,10 +33,15 @@ class PLEA:
             if len(candidates) < 1:
                 continue
 
-            similarity_scores = self.sentence_similarity(text, candidates)
+            similarity_scores = self.batch_sentence_similarities(text, candidates)
             self.extend_enhanced_texts(candidates, similarity_scores, tolerance)
 
         return self.enhanced_texts
+    
+    
+    def get_granular_parts_of_speech(self, text):
+        processed_text = self.text_parser(text)
+        return [token.tag_ for token in processed_text]
 
 
     def insert_mask(self, text, index):
@@ -59,28 +55,23 @@ class PLEA:
         return " ".join(split_text[:index] + masks + split_text[index+1:])
 
 
-    def get_granular_parts_of_speech(self, text):
-        processed_text = self.text_parser(text)
-        return [token.tag_ for token in processed_text]
-
-
-    def fill_mask(self, masked_text, top_k=1, confidence=0.0):
+    def fill_mask(self, masked_text, top_k, confidence):
         outputs = self.mask_filling_model(masked_text)[:top_k]
         confident_outputs = [output['sequence'] for output in outputs if output["score"]>=confidence]
         return confident_outputs
 
 
-    def sentence_similarity(self, source, candidates):
+    def batch_sentence_similarities(self, source, candidates):
         source_embedding = self.sentence_embedding_model.encode(source)
-        candidates_embedding = self.sentence_embedding_model.encode(candidates)
-        return self.cosine_similarity(source_embedding, candidates_embedding)
+        candidates_embeddings = self.sentence_embedding_model.encode(candidates)
+        return self.batch_cosine_similarities(source_embedding, candidates_embeddings)
 
 
-    def cosine_similarity(self, embedding1, embedding2):
-        dot_product = np.dot(embedding2, embedding1.T).squeeze()
-        norm_embedding1 = np.linalg.norm(embedding1)
-        norm_embedding2 = np.sqrt(np.einsum('ij,ij->i', embedding2, embedding2))
-        return dot_product / (norm_embedding1 * norm_embedding2)
+    def batch_cosine_similarities(self, source, candidates):
+        dot_products = np.einsum("ij,j->i", candidates, source)
+        norm_source = np.sqrt(np.einsum('i,i->', source, source))
+        norm_candidates = np.sqrt(np.einsum('ij,ij->i', candidates, candidates))
+        return dot_products / (norm_source * norm_candidates)
 
 
     def extend_enhanced_texts(self, candidates, simiarity_scores, tolerance):
